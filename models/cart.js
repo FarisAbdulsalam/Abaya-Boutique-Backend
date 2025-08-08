ebb2399937181d581f12ae9bbec58fa7d2e3eadd
Faris <farisalmannaei@outlook.com>
2025-08-09 00:52:42 +0300

    added add abaya to cart functionality, get cart functionality, renamed custom.js to cart.js

--- models/cart.js
+++ models/cart.js
@@ -4,7 +4,7 @@ const customAbaya = require("./custom.js");
 const cartSchema = new mongoose.Schema({
   abaya: { type: mongoose.Schema.Types.ObjectId, ref: "Abaya" },
   customAbaya: { type: customAbaya },
-  price: { Number, required: true },
+  price: { type: Number, required: true },
   type: { type: String, enum: ["standard", "custom"], required: true },
 });
 
