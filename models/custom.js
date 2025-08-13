326c64f12a1f75a543e9fba6d64380ba29d62efa
Faris <farisalmannaei@outlook.com>
2025-08-13 04:10:12 +0300

    fixed custom model to work with frontend

--- models/custom.js
+++ models/custom.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const customAbayaSchema = new mongoose.Schema({
   size: { type: String, required: true },
   material: { type: String, required: true },
-  accessory: { type: String, required: true },
+  accessory: { type: [String], required: true },
   colour: { type: String, required: true },
   style: { type: String, required: true },
   comment: String,
